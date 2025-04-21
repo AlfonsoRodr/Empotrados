77aaeb04ff16079881415fcc71bfaf9d88354704
AlfonsoRodr <fonssitorodriguezgutt@gmail.com>
2025-04-21 13:27:35 +0200

    Add the missing doc for MotorLock

--- Proyecto/MainProgram/MotorLock.cpp
+++ Proyecto/MainProgram/MotorLock.cpp
@@ -1,16 +1,35 @@
-#include "motorLock.h"
+/**
+ * @file MotorLock.cpp
+ * @author Raúl Sánchez.
+ * @brief Implementation file for servo motor lock control functions.
+ *
+ * This file provides the implementation of functions declared in MotorLock.h,
+ * allowing control of a servo motor that operates as a locking mechanism.
+ */
+#include "MotorLock.h"
 
 Servo servo;
 
+/**
+ * @brief Initializes the servo motor.
+ *
+ * Attaches the servo to the specified pin and sets it to the locked position.
+ */
 void setupServo(){
-  servo.attach(13); // LO PUSE AHÍ PORQUE EN EL 3 ESTÁ LA HUELLA, PERO LUEGO SE PONE EL NÚMERO DE PIN CORRESPONDIENTE.
+  servo.attach(SERVO_PIN);
   servo.write(SERVO_CLOSE);
 }
 
+/**
+ * @brief Locks the mechanism by moving the servo to the closed position.
+ */
 void closeServo(){
   servo.write(SERVO_CLOSE);
 }
 
+/**
+ * @brief Unlocks the mechanism by moving the servo to the open position.
+ */
 void openServo(){
   servo.write(SERVO_OPEN);
 }
--- Proyecto/MainProgram/MotorLock.h
+++ Proyecto/MainProgram/MotorLock.h
@@ -1,17 +1,42 @@
+/**
+ * @file MotorLock.h
+ * @author Raúl Sánchez.
+ * @brief Header file for controlling a servo motor used as a lock mechanism.
+ *
+ * This module provides basic functions to initialize, open, and close a servo motor
+ * that acts as a locking mechanism. It uses the Arduino Servo library to manage
+ * servo operations.
+ */
+
 #ifndef MOTOR_LOCK_H
 #define MOTOR_LOCK_H
 
 #include <Servo.h>
 
-
 #define SERVO_OPEN -90
 #define SERVO_CLOSE 120
+#define SERVO_PIN 13
 
-
+/**
+ * @brief Initializes the servo motor.
+ *
+ * Attaches the servo motor to the defined pin and prepares it for use.
+ */
 void setupServo();
 
+/**
+ * @brief Closes the servo motor (locks the mechanism).
+ *
+ * Moves the servo to the defined lock position (SERVO_CLOSE).
+ */
 void closeServo();
 
+
+/**
+ * @brief Opens the servo motor (unlocks the mechanism).
+ *
+ * Moves the servo to the defined open position (SERVO_OPEN).
+ */
 void openServo();
 
 #endif
